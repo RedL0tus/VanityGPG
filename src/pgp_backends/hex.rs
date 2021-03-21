acd4fa4b3dd59df77abda5f16787a33904c217e4
Kay Lin <i@v2bv.net>
2021-03-21 07:46:25 -0700

    hex: Fix conditional compilation and add implementation with padded AVX2
    Signed-off-by: Kay Lin <i@v2bv.net>

--- src/pgp_backends/hex.rs
+++ src/pgp_backends/hex.rs
@@ -11,11 +11,12 @@ static TABLE: &[u8; 16] = b"0123456789ABCDEF";
 
 /// SHA-1 binary to hex conversion with SSE4.1
 /// We have a fixed input and output length
-#[cfg(any(target_arch = "x86", target_arch = "x86_64", target_feature = "sse4.1"))]
+#[target_feature(enable = "sse4.1")]
+#[cfg(target_arch = "x86_64")]
 unsafe fn sha1_to_hex_sse41(binary: &[u8], hex: &mut [u8]) {
     let ascii_zero = _mm_set1_epi8(b'0' as i8);
     let nines = _mm_set1_epi8(9);
-    let ascii_a = _mm_set1_epi8((b'a' - 9 - 1) as i8);
+    let ascii_a = _mm_set1_epi8((b'A' - 9 - 1) as i8);
     let and4bits = _mm_set1_epi8(0xf);
 
     let invec = _mm_loadu_si128(binary.as_ptr() as *const _);
@@ -41,6 +42,48 @@ unsafe fn sha1_to_hex_sse41(binary: &[u8], hex: &mut [u8]) {
     hex_fallback(&binary[16..], &mut hex[32..]);
 }
 
+/// SHA-1 binary to hex conversion with SSE4.1
+/// We have a fixed input and output length, and have to pad the input and output
+#[target_feature(enable = "avx2")]
+#[cfg(target_arch = "x86_64")]
+unsafe fn sha1_to_hex_avx2(binary: &[u8], hex: &mut [u8]) {
+    // Preparing padded input and output
+    let mut padded_output: [u8; 64] = [0x0; 64];
+    let mut padded_input: [u8; 32] = [0x0; 32];
+    std::ptr::copy_nonoverlapping(binary.as_ptr(), padded_input.as_mut_ptr(), 20);
+
+    let ascii_zero = _mm256_set1_epi8(b'0' as i8);
+    let nines = _mm256_set1_epi8(9);
+    let ascii_a = _mm256_set1_epi8((b'A' - 9 - 1) as i8);
+    let and4bits = _mm256_set1_epi8(0xf);
+
+    let invec = _mm256_loadu_si256(padded_input.as_ptr() as *const _);
+    let masked1 = _mm256_and_si256(invec, and4bits);
+    let masked2 = _mm256_and_si256(_mm256_srli_epi64(invec, 4), and4bits);
+    // return 0xff corresponding to the elements > 9, or 0x00 otherwise
+    let cmpmask1 = _mm256_cmpgt_epi8(masked1, nines);
+    let cmpmask2 = _mm256_cmpgt_epi8(masked2, nines);
+
+    // add '0' or the offset depending on the masks
+    let masked1 = _mm256_add_epi8(masked1, _mm256_blendv_epi8(ascii_zero, ascii_a, cmpmask1));
+    let masked2 = _mm256_add_epi8(masked2, _mm256_blendv_epi8(ascii_zero, ascii_a, cmpmask2));
+
+    // interleave masked1 and masked2 bytes
+    let res1 = _mm256_unpacklo_epi8(masked2, masked1);
+    let res2 = _mm256_unpackhi_epi8(masked2, masked1);
+
+    // Store everything into the right destination now
+    let base = padded_output.as_mut_ptr();
+    let base1 = base.offset(0) as *mut _;
+    let base2 = base.offset(16) as *mut _;
+    let base3 = base.offset(32) as *mut _;
+    let base4 = base.offset(48) as *mut _;
+    _mm256_storeu2_m128i(base3, base1, res1);
+    _mm256_storeu2_m128i(base4, base2, res2);
+
+    std::ptr::copy_nonoverlapping(padded_output.as_ptr(), hex.as_mut_ptr(), 40);
+}
+
 /// Software implementation of binary to hex
 fn hex_fallback(binary: &[u8], hex: &mut [u8]) {
     for (byte, slots) in binary.iter().zip(hex.chunks_mut(2)) {
@@ -53,10 +96,15 @@ fn hex_fallback(binary: &[u8], hex: &mut [u8]) {
 pub fn sha1_to_hex(binary: &[u8]) -> String {
     let mut result: Vec<u8> = vec![0x0; 40];
 
-    if cfg!(all(target_arch = "x86_64", target_feature = "sse4.1")) {
-        #[cfg(all(target_arch = "x86_64", target_feature = "sse4.1"))]
-        unsafe {
-            sha1_to_hex_sse41(binary, &mut result);
+    // TODO: Support NEON on AArch64
+    if cfg!(target_arch = "x86_64") {
+        #[cfg(target_arch = "x86_64")]
+        if is_x86_feature_detected!("avx2") {
+            unsafe { sha1_to_hex_avx2(binary, &mut result) }
+        } else if is_x86_feature_detected!("sse4.1") {
+            unsafe { sha1_to_hex_sse41(binary, &mut result) }
+        } else {
+            hex_fallback(binary, &mut result);
         }
     } else {
         hex_fallback(binary, &mut result);
@@ -67,18 +115,37 @@ pub fn sha1_to_hex(binary: &[u8]) -> String {
 
 #[cfg(test)]
 mod hex_test {
-    use super::{sha1_to_hex, sha1_to_hex_sse41};
+    use super::sha1_to_hex;
+    #[cfg(target_arch = "x86_64")]
+    use super::{sha1_to_hex_avx2, sha1_to_hex_sse41};
     use hex::encode_upper;
 
     #[test]
+    #[cfg(target_arch = "x86_64")]
     fn test_sha1_to_hex_sse41() {
-        let data: &[u8; 20] = b"0123456789ABCDEFGHIJ";
-        let mut result: Vec<u8> = vec![0x0; 40];
-        unsafe {
-            sha1_to_hex_sse41(data, &mut result);
+        if is_x86_feature_detected!("sse4.1") {
+            let data: &[u8; 20] = b"0123456789ABCDEFGHIJ";
+            let mut result: Vec<u8> = vec![0x0; 40];
+            unsafe {
+                sha1_to_hex_sse41(data, &mut result);
+            }
+            let hex_string = unsafe { String::from_utf8_unchecked(result) };
+            assert_eq!(hex_string, encode_upper(data));
         }
-        let hex_string = unsafe { String::from_utf8_unchecked(result) };
-        assert_eq!(hex_string, encode_upper(data));
+    }
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn test_sha1_to_hex_avx2() {
+        if is_x86_feature_detected!("avx2") {
+            let data: &[u8; 20] = b"0123456789ABCDEFGHIJ";
+            let mut result: Vec<u8> = vec![0x0; 40];
+            unsafe {
+                sha1_to_hex_avx2(data, &mut result);
+            }
+            let hex_string = unsafe { String::from_utf8_unchecked(result) };
+            assert_eq!(hex_string, encode_upper(data));
+        }
     }
 
     #[test]
